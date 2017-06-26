68a958ec2a389f3be3ab17a796e8399c028957cb
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-26 15:09:26 -0500

    placeholder functions

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function Rooms(){
+ //build some rooms
+}
+
+Rooms.prototype.displayRoom = function(){
+  //build display of room information to show when li is selected
+};
+
+function Hotels(){
+  //build a hotel
+}
