c75238d66c4726756c40dfc1cff4d57f62f467d4
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-26 16:24:03 -0500

    build list of hotel rooms and hotela

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function Rooms(roomId,roomType,imgSrc,isVacant,allowedOccupancy,roomLayoutSrc,iceCreamBar,wetBar,poolSide,hotTub,miniBar,fridge,microwave,kitchenette){
+function Room(roomId,roomType,imgSrc,isVacant,allowedOccupancy,roomLayoutSrc,iceCreamBar,wetBar,hotTub,miniBar,fridge,microwave,kitchenette){
   this.roomId = roomId;
   this.roomType = roomType;
   this.imgSrc = imgSrc;
@@ -16,12 +16,22 @@ function Rooms(roomId,roomType,imgSrc,isVacant,allowedOccupancy,roomLayoutSrc,ic
   this.kitchenette = kitchenette;
 }
 
-Rooms.prototype.displayRoom = function(){
+Room.prototype.displayRoom = function(){
   //build display of room information to show when li is selected
 };
 
-function Hotels(hotelImgSrc,hotelLayoutSrc, rooms){
+function Hotel(hotelImgSrc,hotelLayoutSrc, hotelRooms){
   this.hotelImgSrc = hotelImgSrc;
   this.hotelLayoutSrc = hotelLayoutSrc;
-  this.rooms = rooms || [];
+  this.hotelRooms = hotelRooms || [];
 }
+
+var hotelRoomsA = [
+  new Room('2A', 'Executive Suite','placeholder.jpg',true,'10','placeholder2.svg',true,true,true,false,false,false,true)
+  ,new Room('2B', 'Family Suite','placeholder.jpg', true,'7','placeholder2.svg',true,false,true,false,false,false,true)
+  ,new Room('1A', 'Basic Economy','placeholder.jpg', true,'4','placeholder2.svg',false,false,false,false,true,false,false)
+  ,new Room('1B', 'Family Economy','placeholder.jpg', true,'5','placeholder2.svg',false,false,false,false,true,true,false)
+  ,new Room('1C', 'Business Class','placeholder.jpg', true,'3','placeholder2.svg',false,false,true,true,false,true,false)
+];
+
+var hotelA = new Hotel('hotelPlaceholder.jpg','hotelPlaceholder2.svg',hotelRoomsA);
