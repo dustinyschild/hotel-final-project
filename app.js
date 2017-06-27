d8516701f43f97421e5cccb864e54e662ad8c100
Dustin <dyelland@gmail.com>
2017-06-27 13:07:08 -0500

    removed unnecessary precautions and added some images to our room objects

--- app.js
+++ app.js
@@ -75,15 +75,11 @@ function displayRoom() {
 
 Hotel.prototype.randomOccupancy = function(){
   for (var key in this.hotelRooms) {
-    if (this.hotelRooms.hasOwnProperty(key)) { // not sure if necessairy, shouldn't be anything in the prototype that will make issues.
-      var obj = this.hotelRooms[key];
-      for (var property in obj) {
-        if (obj.hasOwnProperty(property)) { // pretty sure this one is unnecesairy as well.
-          if(property === 'isVacant'){
-            if(Math.random() < 0.2){
-              obj[property] = false;
-            }
-          }
+    var obj = this.hotelRooms[key];
+    for (var property in obj) {
+      if(property === 'isVacant'){
+        if(Math.random() < 0.2){
+          obj[property] = false;
         }
       }
     }
@@ -98,8 +94,8 @@ function writeVancanyToLocalStorage(){
 }
 
 var hotelRoomsA = [
-  new Room('2A', 'Executive Suite','500.00','placeholder.jpg',true,'10','placeholder2.svg',true,true,true,false,false,false,true)
-  ,new Room('2B', 'Family Suite','300.00','placeholder.jpg', true,'7','placeholder2.svg',true,false,true,false,false,false,true)
+  new Room('2A', 'Executive Suite','500.00','Pictures/execSuite.jpg',true,'10','placeholder2.svg',true,true,true,false,false,false,true)
+  ,new Room('2B', 'Family Suite','300.00','Pictures/familySuite.jpg', true,'7','placeholder2.svg',true,false,true,false,false,false,true)
   ,new Room('1A', 'Basic Economy','50.00','placeholder.jpg', true,'4','placeholder2.svg',false,false,false,false,true,false,false)
   ,new Room('1B', 'Family Economy','80.00','placeholder.jpg', true,'5','placeholder2.svg',false,false,false,false,true,true,false)
   ,new Room('1C', 'Business Class','100.00','placeholder.jpg', true,'3','placeholder2.svg',false,false,true,true,false,true,false)
